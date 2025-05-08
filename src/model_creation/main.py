e75c4d9bc5f605286ca4b3c2d3f533e1d7092872
anshmenghani <ansh.menghani@gmail.com>
2025-05-08 10:45:11 -0400

    create model

--- src/model_creation/main.py
+++ src/model_creation/main.py
@@ -3,6 +3,19 @@ from tensorflow.keras.layers import Input, Dense, LayerNormalization, GaussianDr
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import MeanSquaredLogarithmicError as MSLE
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+path = ""
+
+def loadData(path):
+    df = pd.read_csv(path)
+    x_labels, y_labels = [], []
+    x = df[x_labels]
+    y = df[y_labels]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+    return x_train, x_test, y_train, y_test
 
 def HurricanePathModel(shape):
     input_layer = Input(shape=shape)
@@ -28,5 +41,8 @@ def HurricanePathModel(shape):
     return closing_price_model
 
 
+x_train, x_test, y_train, y_test = loadData(path)
 hpm = HurricanePathModel((100, 3,)) # change shape later 
 hpm.compile(optimizer=Adam(learning_rate=0.001), loss=MSLE(), metrics=["accuracy"], run_eagerly=False, jit_compile=False, steps_per_execution=1)
+hpm.fit(x=x_train, y=y_train, validation_split=0.20, epochs=100, batch_size=32, shuffle=True, validation_batch_size=16, validation_freq=2)
+hpm.save("src/model")
